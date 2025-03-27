9464ca048f29cf8be31cc7f78b16736f9db57294
Jesse Koch <thejessekoch@gmail.com>
2025-03-26 23:09:47 -0700

    shift demo

--- src/lib/components/forms/DatePicker.svelte
+++ src/lib/components/forms/DatePicker.svelte
@@ -1,20 +1,32 @@
 <script lang="ts">
-	import flatpickr from 'flatpickr';
-	import type { Instance } from 'flatpickr/dist/types/instance';
 	import { onMount } from 'svelte';
+	import { Chrono } from 'chrono-node';
+	import * as luxon from 'luxon';
 
-	let input: HTMLInputElement;
-	let pickr: Instance;
+	let { result = $bindable() } = $props();
+
+	let input = $state('');
+
+	const c = new Chrono();
+
+	$effect(() => {
+		const _result = c.parse(input);
+		const text = _result.pop()?.start?.date().toISOString() || '';
+		const humanText = luxon.DateTime.fromISO(text).toFormat('MMM d, yyyy, h:mm a');
+		input = humanText;
+	});
 
 	onMount(() => {
-		pickr = flatpickr(input, {
-			enableTime: true,
-			dateFormat: 'Y-m-d H:i',
-			defaultDate: new Date()
-		}) as Instance;
+		input = result;
 	});
 </script>
 
-<div class="">
-	<input type="text" placeholder="Select Date" data-input bind:this={input} />
+<div class="grid grid-cols-1 items-center gap-2 lg:grid-cols-[auto_auto]">
+	<input
+		class="input col-span-2 w-full"
+		type="text"
+		bind:value={input}
+		placeholder="e.g. '3pm' or 'yesterday 5pm'"
+	/>
+	<input type="hidden" value={result} />
 </div>
