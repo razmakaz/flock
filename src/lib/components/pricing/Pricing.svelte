42c38a41594ec7213d410f4a1e179a63217ba196
IzykGit <lancehemphill2000@gmail.com>
2025-03-26 21:03:57 -0400

    created start of pricing page

--- src/lib/components/pricing/Pricing.svelte
+++ src/lib/components/pricing/Pricing.svelte
@@ -1,63 +1,23 @@
 <script lang="ts">
 	import { t } from '$lib/translations.svelte';
 
-	// shorten pricing, probably dont need all of this in there
-
 	$: pricing = [
 		{
 			title: $t('pricing.free.title'),
 			frequency: $t('pricing.free.frequency'),
-			bestFor: $t('pricing.free.bestFor'),
-
-			// move these to the pricing page and out of pricing component, this can
-			// be used for the drop downs instead
-			timesheets: [
-				$t('services.timesheet.projectRates'),
-				$t('services.timesheet.miscBillables'),
-				$t('services.timesheet.timeclock'),
-				$t('services.timesheet.taskTracking')
-			],
-			invoicing: [
-				$t('services.invoicing.contractManagement'),
-				$t('services.invoicing.lateFees'),
-				$t('services.invoicing.remittanceAdvice'),
-				$t('services.invoicing.revisions'),
-				$t('services.invoicing.creditMemos')
-			]
+			bestFor: $t('pricing.free.bestFor')
 		},
 		{
 			title: $t('pricing.license.title'),
 			frequency: $t('pricing.license.frequency'),
 			bestFor: $t('pricing.license.bestFor'),
-			inclusions: $t('pricing.license.inclusions'),
-
-			timesheets: {
-				overtime: $t('services.timesheet.overtime'),
-				liveReporting: $t('services.timesheets.liveReporting'),
-				forecasting: $t('services.timesheets.forecasting')
-			},
-			invoicing: {
-				forcastingTools: $t('services.invoicing.forecastingTools'),
-				dunningNotices: $t('services.invoicing.dunningNotices'),
-				disputeHandling: $t('services.invoicing.disputHandling')
-			}
+			inclusions: $t('pricing.license.inclusions')
 		},
 		{
 			title: $t('pricing.subscription.title'),
 			frequency: $t('pricing.subscription.frequency'),
 			bestFor: $t('pricing.subscription.bestFor'),
-			inclusions: $t('pricing.subscriptions.inclusions'),
-
-			timesheets: {
-				teams: $t('teams'),
-				scheduleGeneration: $t('services.timesheet.scheduleGeneration'),
-				shiftApprovals: $t('services.timesheet.shiftApprovals'),
-				textReminders: $t('services.timesheet.textReminders')
-			},
-			invoicing: {
-				mailingInvoices: $t('services.invoicing.mailingInvoices')
-			},
-			ai: {}
+			inclusions: $t('pricing.subscriptions.inclusions')
 		}
 	];
 </script>
@@ -69,26 +29,6 @@
 		>
 			<h2 class="text-3xl">{item.title}</h2>
 			<p class="text-lg">{item.frequency}</p>
-
-			<!-- TODO: Scrap this, implement something more concise and smaller -->
-
-			<!-- <div>
-				<div class="text-md flex flex-col gap-2">
-					<h3>TimeSheets</h3>
-					{#if item.timesheets}
-						{#each item.timesheets as feature}
-							<p>{feature}</p>
-						{/each}
-					{/if}
-				</div>
-				<div>
-					{#if item.invoicing}
-						{#each item.invoicing as feature}
-							<p>{feature}</p>
-						{/each}
-					{/if}
-				</div>
-			</div> -->
 		</div>
 	{/each}
 {/snippet}
--- src/routes/(anon)/(landing)/pricing/+page.svelte
+++ src/routes/(anon)/(landing)/pricing/+page.svelte
@@ -1,7 +1,82 @@
 <script lang="ts">
 	import Pricing from '$lib/components/pricing/Pricing.svelte';
 	import { t } from '$lib/translations.svelte';
+	import Icon from '@iconify/svelte';
+
+	let state = $state({
+		openCategories: []
+	});
+
+	const _pricing = {
+		tiers: [
+			{
+				title: $t('pricing.free.title'),
+				monthlyPrice: 0,
+				annualPrice: 0
+			},
+			{
+				title: $t('pricing.license.title'),
+				monthlyPrice: 10,
+				annualPrice: 100
+			},
+			{
+				title: $t('pricing.subscription.title'),
+				monthlyPrice: 20,
+				annualPrice: 200
+			}
+		],
+		categories: [
+			{
+				name: $t('services.timesheets.title'),
+				stars: {
+					free: 2,
+					license: 3,
+					subscription: 5
+				},
+				items: [
+					{
+						name: $t('services.timesheet.projectRates'),
+						free: 5,
+						license: 15,
+						subscription: 40,
+						tooltip: $t('services.timesheet.projectRatesTooltip')
+					},
+					{
+						name: $t('services.timesheet.miscBillables'),
+						free: true,
+						license: true,
+						subscription: true,
+						tooltip: $t('services.timesheet.miscBillablesTooltip')
+					}
+				]
+			}
+		]
+	};
+
+	const handleToggleCategory = (category: string) => {
+		if (state.openCategories.includes(category)) {
+			state.openCategories = state.openCategories.filter((c) => c !== category);
+		} else {
+			state.openCategories = [...state.openCategories, category];
+		}
+	};
 </script>
+
+{#snippet Rating(stars)}
+	<div class="rating">
+		{#each Array.from({ length: 5 }, (_, i) => i + 1) as star, i}
+			<div class="mask mask-star" aria-label="{star} star" aria-current={i === stars}></div>
+		{/each}
+	</div>
+{/snippet}
+
+{#snippet ItemValue(value: boolean | number)}
+	{#if typeof value == 'boolean'}
+		<Icon icon={value ? 'check' : 'close'} />
+	{:else}
+		{value}
+	{/if}
+{/snippet}
 
 <main class="relative min-h-screen px-4 py-24">
 	<div
@@ -21,5 +96,34 @@
 
 		<!-- TODO: Implement features and dropdowns here with comparisons
         TODO: Create comparisons grid -->
+
+		<!-- {@render TimeSheetSection(pricing)} -->
+
+		<div class="grid grid-cols-[1fr_auto_auto_auto] gap-8">
+			{#each _pricing.categories as category}
+				<button
+					class=" text-left {category.tooltip ? 'underline' : ''}"
+					onclick={() => handleToggleCategory(category.name)}>{category.name}</button
+				>
+				{@render Rating(category.stars.free)}
+				{@render Rating(category.stars.license)}
+				{@render Rating(category.stars.subscription)}
+				{#if state.openCategories.includes(category.name)}
+					{#each category.items as item}
+						<div class="flex">
+							<div class="tooltip">
+								<div class="tooltip-content">
+									<p class="">{category.tooltip}</p>
+								</div>
+								<p class="">{item.name}</p>
+							</div>
+						</div>
+						<p class="">{item.free}</p>
+						<p class="">{item.license}</p>
+						<p class="">{item.subscription}</p>
+					{/each}
+				{/if}
+			{/each}
+		</div>
 	</div>
 </main>
