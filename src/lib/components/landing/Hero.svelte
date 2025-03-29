d5ba4b088acab86b62e59adece08f4e7587a2b65
Jesse Koch <thejessekoch@gmail.com>
2025-03-28 17:22:59 -0700

    gradients!

--- src/lib/components/landing/Hero.svelte
+++ src/lib/components/landing/Hero.svelte
@@ -6,6 +6,9 @@
 	import Icon from '@iconify/svelte';
 	import TimesheetsDemo from '../demos/TimesheetsDemo.svelte';
 	import DemoContainer from '../demos/DemoContainer.svelte';
+	import { onMount } from 'svelte';
+	import { Gradient } from '../stripebg/Gradient';
+	import App from '$lib/stores/App';
 
 	let input = $state('');
 	const disabled = $derived(!emailRegex.test(input));
@@ -23,75 +26,111 @@
 		url.searchParams.append('email', input);
 		goto(url.href);
 	};
+
+	onMount(() => {
+		const darkGradient = new Gradient();
+		const lightGradient = new Gradient();
+		darkGradient.initGradient('#dark-bg-canvas');
+		lightGradient.initGradient('#light-bg-canvas');
+		console.log($App.theme);
+	});
 </script>
 
 <section class="hero relative flex max-w-svw justify-center">
-	<div class="absolute inset-0 flex -translate-y-10 items-center justify-center mix-blend-lighten">
-		<iframe
-			src="https://my.spline.design/discover-39661a5ea472992df13239fc498c95a2/"
-			frameborder="0"
-			class="w-full mix-blend-lighten"
-			style="height: 140vh; width: 90vw;"
-		></iframe>
-	</div>
-	<div class="hero-content relative z-10 flex flex-col">
-		<div
-			class="rounded-b-field absolute
-			 inset-0
-			-translate-y-1/2
-			bg-gradient-to-r
-			 from-indigo-600
-			to-violet-600 opacity-20 blur-2xl
-			 transition-all duration-1000
-			ease-out
-		"
-			style="height: 100vh;"
-		></div>
-
-		<div
-			class="z-10 flex flex-col items-center justify-center gap-4 px-4 py-8 text-center lg:py-24"
-		>
-			<span class="bg-base-100 relative inline rounded-md p-4">
-				<div
-					class="absolute -z-10 rounded-md bg-gradient-to-r from-indigo-400 to-violet-500"
-					style="inset: 4px -4px;"
-				></div>
-				<h1
-					class="floc inline-block bg-gradient-to-r from-indigo-400 to-violet-500 bg-clip-text text-5xl font-bold text-transparent"
-				>
+	<canvas
+		class="absolute inset-0 -translate-y-1/2 {$App.theme === 'dark' ? '' : 'hidden'} "
+		id="dark-bg-canvas"
+		style="width: 100vw;"
+	></canvas>
+	<canvas
+		class="absolute inset-0 -translate-y-1/2 {$App.theme === 'dark' ? 'hidden' : ''} "
+		id="light-bg-canvas"
+		style="width: 100vw;"
+	></canvas>
+	<div class="hero-content relative z-20 flex flex-col">
+		<div class="relative z-10">
+			<div
+				class="z-10 flex flex-col items-center justify-center gap-4 px-4 py-8 text-center lg:py-24"
+			>
+				<h1 class="floc inline-block text-6xl font-black">
 					{$t('landing.hero.title')}
 				</h1>
-			</span>
-			<h2 class="py-6 text-center text-2xl leading-12 tracking-wide">
-				<span class="bg-base-100 inline rounded-md p-2">
+				<h2 class="py-6 text-center text-2xl leading-12 tracking-wide">
 					{@html $t('landing.hero.subtitle')}
-				</span>
-			</h2>
-			<div class="flex justify-center">
-				<div class="join join-vertical lg:join-horizontal bg-neutral rounded-sm">
-					<div>
-						<label class="input validator join-item w-full">
-							<Icon icon="ic:baseline-email" width={24} height={24} />
-							<input
-								bind:value={input}
-								name="email"
-								class=" ring-transparent"
-								type="email"
-								placeholder="email@site.com"
-								required
-							/>
-						</label>
-						<div class="validator-hint hidden w-full">{$t('landing.hero.validator')}</div>
+				</h2>
+				<div class="flex justify-center">
+					<div class="join join-vertical lg:join-horizontal bg-neutral rounded-sm">
+						<div>
+							<label class="input validator join-item w-full">
+								<Icon icon="ic:baseline-email" width={24} height={24} />
+								<input
+									bind:value={input}
+									name="email"
+									class=" ring-transparent"
+									type="email"
+									placeholder="email@site.com"
+									required
+								/>
+							</label>
+							<div class="validator-hint hidden w-full">{$t('landing.hero.validator')}</div>
+						</div>
+						<button onclick={handleSubmit} {disabled} class="btn btn-primary join-item"
+							>{$t('landing.hero.cta')}</button
+						>
 					</div>
-					<button onclick={handleSubmit} {disabled} class="btn btn-primary join-item"
-						>{$t('landing.hero.cta')}</button
-					>
 				</div>
 			</div>
 		</div>
-
 		<div class="relative z-10 p-8">
 			<DemoContainer />
 		</div>
 	</div>
 </section>
+
+<style>
+	#dark-bg-canvas {
+		--gradient-color-1: #3b82f6; /* Tailwind CSS blue-500 */
+		--gradient-color-2: #5e46a8;
+		--gradient-color-3: #5c7abf;
+		--gradient-color-4: #000000;
+
+		/* Show only the triangle area */
+		mask-image: linear-gradient(165deg, black 85%, transparent 65%);
+		mask-size: 100% 100%;
+		mask-repeat: no-repeat;
+
+		/* For WebKit-based browsers */
+		-webkit-mask-image: linear-gradient(170deg, black 50%, transparent 80%);
+		-webkit-mask-size: 100% 100%;
+		-webkit-mask-repeat: no-repeat;
+
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+	}
+
+	#light-bg-canvas {
+		--gradient-color-1: #3b82f6; /* Tailwind CSS blue-500 */
+		--gradient-color-2: #5e46a8;
+		--gradient-color-3: #5c7abf;
+		--gradient-color-4: #ffffff;
+
+		/* Show only the triangle area */
+		mask-image: linear-gradient(165deg, black 85%, transparent 65%);
+		mask-size: 100% 100%;
+		mask-repeat: no-repeat;
+
+		/* For WebKit-based browsers */
+		-webkit-mask-image: linear-gradient(170deg, black 50%, transparent 80%);
+		-webkit-mask-size: 100% 100%;
+		-webkit-mask-repeat: no-repeat;
+
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+	}
+</style>
