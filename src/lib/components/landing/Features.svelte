c46faadca8333b34a84865e1cab82d0fa6581790
IzykGit <lancehemphill2000@gmail.com>
2025-04-02 20:14:43 -0400

    fixed cards on features

--- src/lib/components/landing/Features.svelte
+++ src/lib/components/landing/Features.svelte
@@ -38,33 +38,34 @@
 </script>
 
 {#snippet FeatureCards(section)}
-	<div class="relative grid cursor-pointer grid-cols-1 gap-4 md:grid-cols-2">
+	<div class="relative grid min-h-[450px] cursor-pointer grid-cols-1 gap-4 md:grid-cols-2">
 		{#each section as feature}
-			<div
-				class="bg-base-200 cursor-pointer bg-gradient-to-br from-violet-500 to-violet-600 p-0 shadow-2xl duration-200 hover:scale-105 hover:p-4"
+			<button
+				class="bg-base-100 relative h-full w-full cursor-pointer transition-all duration-300 *:rounded-sm"
+				onclick={() => {
+					selectedFeature.title = feature.title;
+					selectedFeature.body = feature.body;
+				}}
 			>
-				<button
-					class=" bg-base-200 flex h-full w-full flex-none cursor-pointer flex-col items-start p-4
-				 "
-					onclick={() => {
-						selectedFeature.title = feature.title;
-						selectedFeature.body = feature.body;
-					}}
+				<div
+					class="absolute inset-[1px] z-0 bg-gradient-to-br from-violet-300 to-indigo-500 transition-all duration-300 hover:-inset-1"
+				></div>
+				<div
+					class="bg-base-200 pointer-events-none relative z-10 flex h-full w-full flex-col items-center justify-center p-6"
 				>
-					<div class="mb-4 text-blue-400" aria-hidden="true"></div>
 					<h3 class="mb-2 text-lg font-semibold break-normal text-white sm:text-xl">
 						{feature.title}
 					</h3>
 					<p class="text-sm text-gray-400">{feature.body}</p>
-				</button>
-			</div>
+				</div>
+			</button>
 		{/each}
 	</div>
 {/snippet}
 
 <section class="bg-primary h-fit px-4 py-32">
 	<div
-		class="bg-base-100 mx-auto flex min-h-[500px] w-full max-w-6xl flex-col justify-between gap-8 p-16 sm:flex-row"
+		class="bg-base-100 mx-auto flex h-fit w-full max-w-7xl flex-col justify-between gap-8 p-16 sm:flex-row"
 	>
 		<div class="flex w-1/2 flex-col justify-between">
 			<div class="flex flex-col gap-2">
